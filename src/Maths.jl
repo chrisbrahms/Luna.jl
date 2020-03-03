1017c7220b3500626353d3a28f323d4f54c17c03
chrisbrahms <38351086+chrisbrahms@users.noreply.github.com>
2020-03-03 08:18:13 +0000

    fwhm function

--- src/Maths.jl
+++ src/Maths.jl
@@ -4,6 +4,7 @@ import LinearAlgebra
 import SpecialFunctions: erf, erfc
 import StaticArrays: SVector
 import FFTW
+import Roots: fzero
 
 "Calculate derivative of function f(x) at value x using finite differences"
 function derivative(f, x, order::Integer)
@@ -52,6 +53,39 @@ end
 
 function rms_width(x::Vector, y; dim = 1)
     return sqrt.(moment(x, y, 2, dim = dim) - moment(x, y, 1, dim = dim).^2)
+end
+
+function fwhm(x, y, method=:spline, baseline=false)
+    if baseline
+        val = minimum(y) + 0.5*(maximum(y) - minimum(y))
+    else
+        val = 0.5*maximum(y)
+    end
+    if method in (:spline, :nearest)
+        xmax = x[argmax(y)]
+        left, right = try
+            left = maximum(x[(x .< xmax) .& (y .< val)])
+            right = minimum(x[(x .> xmax) .& (y .< val)])
+            (method == :nearest) && return abs(right - left)
+            left, right
+        catch
+            return NaN
+        end
+        #spline method
+        try
+            spl = CSpline(x, y)
+            lb = xmax - 2*(xmax-left)
+            ub = xmax + 2*(right-xmax)
+            f(x) = spl(x) - val
+            lfine = fzero(f, lb, xmax)
+            rfine = fzero(f, xmax, ub)
+            return abs(right - left)
+        catch
+            return NaN
+        end
+    else
+        error("Unknown FWHM method $method")
+    end
 end
 
 """
